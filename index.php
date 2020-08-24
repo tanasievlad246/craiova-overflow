56b711b7d8148946ce4c8f7c31b1e68e2ce3cdd2
vlad <tanasievladcristian@gmail.com>
2020-08-24 14:26:49 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include './src/components/header.php'; ?>
+
+<h1>Modified with code</h1>
+
+
+<?php include './src/components/footer.php'; ?>
---
+++ src/components/nav.php
@@ -0,0 +1,25 @@
+<div class="container-fluid">
+	<nav class="navbar navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php">Craiova Overflow</a>
+		<div> 
+			<form action="src/includes/login.inc.php" method="POST" class="form-inline float-left mr-2"> 
+				<div class="form-group">
+    				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 	placeholder="email"required>	
+  				</div>
+  				<div class="form-group ml-1">
+    				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="password" required>
+  				</div>
+  				<button type="submit" class="btn btn-primary ml-2">Login</button>
+			</form>
+			
+			<div class="float-left">
+				<form action="" class="float-left">
+			 		<input type="submit" name="Register" value="Register" class="btn btn-outline-success" href="register.php" />
+			 	</form>
+				<form action="" class="float-left ml-2">
+			 		<input type="submit" name="Logout" value="Logout" class="btn btn-outline-success" href="register.php" />
+			 	</form>
+			</div>
+		</div>
+	</nav>
+</div>
